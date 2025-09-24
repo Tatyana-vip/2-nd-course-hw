844dfdae64ca64395d1d6ab28228efd5856da515
Tatyana <tvp-2020@mail.ru>
2025-09-24 09:19:00 +0300

    Creating a js folder

---
+++ Homework2/js/script.js
@@ -0,0 +1,83 @@
+// Задание 1
+
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+
+const release = 2007;
+alert(`Год выпуска первого iPhone ${release}`);
+
+// Задание 3
+
+const creator = 'Брендан Айк';
+alert(`Создатель языка JavaScript ${creator}`);
+
+// Задание 4
+
+let b = 10;
+let c = 2;
+let d = b + c;
+alert(d);
+d = b - c;
+alert(d);
+d = b * c;
+alert(d);
+d = b / c;
+alert(d);
+
+// Задание 5
+
+let result = c**5;
+alert(result);
+
+// Задание 6
+
+a = 9;
+b = 2;
+c = a / b;
+alert(c);
+
+// Задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+alert(num);
+
+// Задание 8
+
+let age = prompt('Сколько Вам лет?');
+alert (age);
+
+// Задание 9
+
+const user = {
+name: 'Василий',
+age: 25,
+isAdmin: false
+};
+
+// Задание 10
+
+let userName = prompt('Как Вас зовут?');
+alert(`Привет, ${userName}!`);
+
+// Дополнительное задание
+
+let number = prompt('Загадайте любое число');
+let doubledNumber = Number(number) * 2;
+alert(doubledNumber);
+let addTen = doubledNumber + 10;
+alert(addTen);
+let dividedByTwo = addTen / 2;
+alert(dividedByTwo);
+let finalResult = dividedByTwo - Number(number);
+alert(finalResult);
+alert('Ответ равен 5');
